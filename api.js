f807213c2dc77ed6b6809e9d1ada6f1c49506d13
yuiharadaa <0429.yuiyui@gmail.com>
2025-08-31 20:03:56 +0900

    単一の従業員

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const API = 'https://script.google.com/macros/s/AKfycbxkjGfYoRaY9fLuVgpVQaIrtIGs
 
 async function fetchAndDisplayEmployees() {
   try {
-    const response = await fetch(API);
+    const response = await fetch(`${API}?action=employee.list`);
     if (!response.ok) {
       throw new Error(`HTTPエラー: ${response.status}`);
     }
@@ -23,6 +23,24 @@ async function fetchAndDisplayEmployees() {
     console.error('取得エラー:', error);
   }
 }
+
+async function getEmployeeById(id) {
+  const empSheet = SpreadsheetApp.getActiveSpreadsheet().getSheets()[1];
+  const last_row = empSheet.getLastRow();
+  const empRange = empSheet.getRange(2, 1, last_row - 1, 2); // ヘッダ除外
+  const values = empRange.getValues();
+
+  for (let i = 0; i < values.length; i++) {
+    if (String(values[i][0]) === String(id)) {
+      return {
+        id: values[i][0],
+        name: values[i][1]
+      };
+    }
+  }
+  return null; // 見つからなかった場合
+}
+
 
 // ページ読み込み時に実行
 window.addEventListener('DOMContentLoaded', fetchAndDisplayEmployees);
